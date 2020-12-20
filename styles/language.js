f20fb603e24732640791e965c873fbe9ceca011d
Nilesh Aditya <nileshaditya2000@gmail.com>
2020-12-20 06:59:06 +0530

    responsive

---
+++ styles/language.js
@@ -0,0 +1,62 @@
+const regBtn = document.querySelector(".register-btn");
+
+const obj = {
+    "english": {
+        "name": "Name:",
+        "age": "Age:",
+        "uid": "Aadhar No:",
+        "sex": {
+            "title": "Gender:",
+            "male": "Male",
+            "female": "Female",
+            "other": "Other"
+        },
+        "med-prac": {
+            "title": "Is a medical practitioner?: ",
+            "y": "Yes",
+            "n": "No"
+        },
+        "address": "Address:",
+        "state": "State:",
+        "city": "City:",
+        "pin": "Pin-Code:",
+        "mn": "Mobile-No:",
+        "an": "Alternate-No:",
+        "vac ": {
+            "title": "Vaccination Preference:",
+            "home": "At home",
+            "centre": "At Centre"
+        }
+    },
+    "hindi": {
+        "name": "नाम:",
+        "age": "आयु:",
+        "uid": "आधार कार्ड संख्या:",
+        "sex": {
+            "title": "लिंग:",
+            "male": "पुरुष",
+            "female": "महिला",
+            "other": "अन्य"
+        },
+        "med-prac": {
+            "title": "क्या आप चिकित्साकर्मी है?: ",
+            "y": "हाँ",
+            "n": "ना"
+        },
+        "address": "पता:",
+        "state": "राज्य:",
+        "city": "शहर:",
+        "pin": "पिन कोड:",
+        "mn": "मोबाइल नम्बर:",
+        "an": "अतिरिक्त मोबाइल नम्बर:",
+        "vac ": {
+            "title": "टीकाकरण वरीयता:",
+            "home": "घर पे",
+            "centre": "केंद्र पे"
+        }
+    }
+};
+
+regBtn.addEventListener("click", e => {
+    
+});
