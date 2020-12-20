176fc7d608be10cfbef88b1e7007b1de2f227f84
Nilesh Aditya <nileshaditya2000@gmail.com>
2020-12-20 19:54:14 +0530

    hosting....

--- app.js
+++ app.js
@@ -5,19 +5,27 @@ app.listen(2020, () => {
     console.log("server running");
 });
 
-// app.use(express.static(""));
+app.use(express.static(__dirname+ ""));
 // app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 app.set('views', '');
 
 app.get('/', (req, res) => {
-    res.render("./html/index.html", { root: __dirname });
+    res.sendFile("./html/index.html", { root: __dirname });
 });
 app.get('/home', (req, res) => {
     res.redirect('/');
 });
 
-app.get("/register.html", (req, res) => { 
-    res.sendFile("/html/register");
+app.get('/index.html', (req, res) => {
+    res.redirect('/');
+});
+
+app.get("/register.html", (req, res) => {
+    res.sendFile("./html/register.html", { root: __dirname });
+});
+
+app.get("/admin-login.html", (req, res) => {
+    res.sendFile(__dirname + "/html/admin-login.html");
 });
